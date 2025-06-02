833d67d598f8fcb3a0147fdbf618a6fd795b32a8
techninja8 <tosinoyinboblessed@gmail.com>
2025-06-02 15:23:24 +0100

    Added README.md

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,13 @@
-pub mod sp1_plug;
+pub mod sp1;
+
+pub use sp1::{
+    Sp1Plug,
+    Sp1PlugConfig,
+    Sp1ProofType,
+    Sp1PlugError,
+};
+
+// Re-export commonly used types for convenience
+pub type SP1Proof = Sp1ProofType;
+pub type SP1Config = Sp1PlugConfig;
+pub type SP1Error = Sp1PlugError;
