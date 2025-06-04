1717f8105f5ae445f3992eb1994e2e9ac6749edb
techninja8 <tosinoyinboblessed@gmail.com>
2025-06-04 17:49:51 +0100

    (update)

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,1 @@
-pub mod sp1;
-
-pub use sp1::{
-    Sp1Plug,
-    Sp1PlugConfig,
-    Sp1ProofType,
-    Sp1PlugError,
-};
-
-// Re-export commonly used types for convenience
-pub type SP1Proof = Sp1ProofType;
-pub type SP1Config = Sp1PlugConfig;
-pub type SP1Error = Sp1PlugError;
+So far, we have built Frostgate's Chain Abstraction Layer (frostgate-icap), with a basic infrastructure for interacting with different chains, right now we've got
